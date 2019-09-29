74f52bf6691ea8e8357353d277b243522d0d3009
artidex <artidex028@gmai.com>
2019-09-29 03:32:16 +0300

    fix first task

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 
 let money = parseInt(prompt('Enter your month budget', 'Insert the number')),
     currentTime = new Date(),
-    time = prompt("Введите дату в формате YYYY-MM-DD", currentTime),
-    firstQ = prompt('Введите обязательную статью расходов в этом месяце', 'введите слово'),
-    secondQ = parseInt(prompt('Во сколько обойдется?', 'введите число'));
+    time = prompt("Введите дату в формате YYYY-MM-DD", currentTime);
+    
 
 const appData = {
     budget: money,
-    timeData: time,
+    //timeData: time,
     expenses: {
-        firstA: firstQ, 
-        secondA: secondQ,
+
     },
     optionalExpenses:{
 
@@ -19,11 +17,30 @@ const appData = {
     income:[
 
     ],
-    savings: Boolean,
+    savings: false,
 
 };
 
-let calcSum = ((appData.budget - appData.expenses.secondA) / 30);
 
+for(let i = 0; i < 2; i++){
+    let a = prompt('Введите обязательную статью расходов в этом месяце1', 'введите слово'),
+        b = parseInt(prompt('Во сколько обойдется1?', 'введите число'));
 
-alert (calcSum.toFixed(3));
+    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && (typeof(b) != null
+    && a !='' && b!= '' & a.length < 50)){
+        appData.expenses[a] = b; // а станет свойством б станет значением
+    } else{
+            // прописать возврат к новому циклу
+    }
+};
+
+appData.moneyPerDay = appData.budget.toFixed(3) / 30;
+//let calcSum = (calc / 30);
+alert('Ежедневній бюджет' + appData.moneyPerDay);
+console.log(appData);
+
+if(appData.moneyPerDay < 100){
+    console.log("na podsosah");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("middle");
+}
